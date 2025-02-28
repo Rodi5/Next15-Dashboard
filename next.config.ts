effc61876cf2fe531579f8eeb5daf6a959407bda
rodi <rodi.shammout1@gmail.com>
2025-02-28 17:06:49 +0200

    Update project

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/Next15-Dashboard',
   experimental: {
     optimizeCss: true,
     scrollRestoration: true,
@@ -12,11 +10,31 @@ const nextConfig: NextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     minimumCacheTTL: 60,
-    unoptimized: true,
   },
   compress: true,
   reactStrictMode: true,
-  
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, s-maxage=86400, stale-while-revalidate=3600",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
